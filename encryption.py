419419161265b04b4f7735747444af6c93827ca3
Jordan Adriano <56409204+jordanadrianoo@users.noreply.github.com>
2020-02-10 08:08:25 -0600

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -2,6 +2,7 @@
 # Jordan Adriano
 # jan 9 2020
 # This code will simply take a long string and swap each letter with their associated encryption
+
 original=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9"," "]
 
 rot_01 = ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "1", "2", "3", "4", "5", "6", "7", "8", "9", " "]
