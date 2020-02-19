94439099c4ab98a04bf0dc97c9b9da486defe0f9
Jordan Adriano <56409204+jordanadrianoo@users.noreply.github.com>
2020-02-19 06:19:53 -0600

    Add files via upload

---
+++ encryption data.py
@@ -0,0 +1,15 @@
+#encryption code
+# Jordan Adriano
+# jan 9 2020
+
+orginal= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9"," "]
+rot_15 = ["p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "1", "2", "3", "4", "5", "6", "7", "8", "9"," "]
+
+
+
+info="this is information that need to be encrypted. use any means, but do not show anybody."
+new_info=""
+
+
+for x in (range(len(info))): #range of the array orginal
+    print(info[x])
